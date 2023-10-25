18e2c3341210f30e2291dcde3476a03bfe66e3cd
szymqn <koodziejskisz@outlook.com>
2023-10-25 14:03:01 +0200

    Added test output in funs, used Manager mp

--- lab3/funcs.py
+++ lab3/funcs.py
@@ -4,13 +4,17 @@ import time
 
 def worker(output):
     name = current_process().name
-    output.put(f"{name} Starting")
+    print(f"{name} Starting")  # Add print statements here
+    output.append(f"{name} Starting")
     time.sleep(2)
-    output.put(f"{name} Exiting")
+    print(f"{name} Exiting")  # Add print statements here
+    output.append(f"{name} Exiting")
 
 
 def service(output):
     name = current_process().name
-    output.put(f"{name} Starting")
+    print(f"{name} Starting")  # Add print statements here
+    output.append(f"{name} Starting")
     time.sleep(3)
-    output.put(f"{name} Exiting")
+    print(f"{name} Exiting")  # Add print statements here
+    output.append(f"{name} Exiting")
