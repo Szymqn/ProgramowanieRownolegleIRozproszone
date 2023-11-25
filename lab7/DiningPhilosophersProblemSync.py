64ba9d114edfb06247da80a742b9574705794933
szymqn <koodziejskisz@outlook.com>
2023-11-25 22:45:16 +0100

    Implemented avoid locks

--- lab7/DiningPhilosophersProblemSync.py
+++ lab7/DiningPhilosophersProblemSync.py
@@ -9,6 +9,8 @@ class PhilosopherParallel(threading.Thread):
         self.index = index
         self.left_fork = left_fork
         self.right_fork = right_fork
+        self.holding_left_fork = False
+        self.holding_right_fork = False
 
     def run(self):
         for _ in range(5):
@@ -21,24 +23,44 @@ class PhilosopherParallel(threading.Thread):
 
     def eat(self):
         print(f"Philosopher {self.index} is hungry and trying to pick up forks.")
-        with self.left_fork, self.right_fork:
-            print(f"Philosopher {self.index} is eating.")
-            time.sleep(random.uniform(1, 3))
+
+        while True:
+            fork1, fork2 = self.left_fork, self.right_fork
+
+            with fork1:
+                if not self.holding_left_fork:
+                    self.holding_left_fork = True
+
+                    with fork2:
+                        if not self.holding_right_fork:
+                            self.holding_right_fork = True
+                            break
+                        else:
+                            self.holding_left_fork = False
+
+            time.sleep(random.uniform(0, 1))
+
+        print(f"Philosopher {self.index} is eating.")
+        time.sleep(random.uniform(1, 3))
+
+        self.holding_left_fork = False
+        self.holding_right_fork = False
         print(f"Philosopher {self.index} finished eating.")
 
 
-num_philosophers = 5
-forks = [threading.Lock() for _ in range(num_philosophers)]
-philosophers = []
+if __name__ == "__main__":
+    num_philosophers = 5
+    forks = [threading.Lock() for _ in range(num_philosophers)]
+    philosophers = []
 
-for i in range(num_philosophers):
-    left_fork = forks[i]
-    right_fork = forks[(i + 1) % num_philosophers]
-    philosopher = PhilosopherParallel(i, left_fork, right_fork)
-    philosophers.append(philosopher)
+    for i in range(num_philosophers):
+        left_fork = forks[i]
+        right_fork = forks[(i + 1) % num_philosophers]
+        philosopher = PhilosopherParallel(i, left_fork, right_fork)
+        philosophers.append(philosopher)
 
-for philosopher in philosophers:
-    philosopher.start()
+    for philosopher in philosophers:
+        philosopher.start()
 
-for philosopher in philosophers:
-    philosopher.join()
+    for philosopher in philosophers:
+        philosopher.join()
